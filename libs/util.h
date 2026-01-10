7478c6cff530a524b887180a74afe08c1b1602f0
Tiago <tiagodongo521@gmail.com>
2026-01-10 20:44:33 -0100

    feat: add unit tests, fix warnings and update build files
    - Add test functions for:
      - createQuestionList
      - emptyQuestionList
      - createQuestion
      - addNewQuestion
      - removeQuestion
    - Fix warnings for implicit declarations (strlen, isspace) in util.c
    - Update action to create directories and run tests
    - Fix code issues in quiz.c
    - Include missing libraries

--- libs/util.h
+++ libs/util.h
@@ -8,6 +8,8 @@
 
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
+#include<ctype.h>
 
 int validateInt(const char *prompt);
 int validateString(const char *prompt, char *destination, int maxLenght);
